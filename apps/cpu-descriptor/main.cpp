f576bda5ed6d67b80ba8996921f4555d92c0f50f
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-11-13 22:40:13 +0700

    cpu-utils initial code

---
+++ apps/cpu-descriptor/main.cpp
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+int main(/*int argc, char* argv[]*/) {
+    return 0;
+}
---
+++ src/cpu-utils.cpp
@@ -0,0 +1,5 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
