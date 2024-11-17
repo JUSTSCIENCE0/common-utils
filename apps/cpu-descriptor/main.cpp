4ca472948cb905b9ec822c674099261ebe5827c6
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-11-17 21:35:33 +0700

    cpu utils instructions families for x86_64

--- apps/cpu-descriptor/main.cpp
+++ apps/cpu-descriptor/main.cpp
@@ -2,7 +2,35 @@
 // Email: yakovmen62@gmail.com
 //
 // License: MIT
+#include <cpu-utils.h>
+
+#include <iostream>
+#include <bitset>
+#include <stdint.h>
+
+using std::cout, std::endl;
+
+void print_about(const char* prog_name) {
+    cout << prog_name << endl;
+    cout << "Copyright (c) 2024, Yakov Usoltsev" << endl;
+    cout << "Email: yakovmen62@gmail.com" << endl;
+    cout << "License: MIT" << endl;
+    cout << endl;
+}
+
+void print_all_arch_supported_sets() {
+    using namespace CU;
+    using std::bitset;
+
+    cout << "All Instructions sets supported for this architecture:" << endl;
+    for (int ins_fam = E_INFAM_BEGIN; ins_fam < E_INFAM_END; ins_fam++) {
+        cout << "Instructions family #" << ins_fam << "\t" << STR_INSTRUCTIONS_FAMILY[ins_fam] << ",\t";
+        cout << "SUPPORT_FLAG is " << bitset<32>(1ll << ins_fam) << endl;
+    }
+}
 
 int main(/*int argc, char* argv[]*/) {
+    print_about("cpu_descriptor");
+    print_all_arch_supported_sets();
     return 0;
 }
--- include/cpu-utils.h
+++ include/cpu-utils.h
@@ -4,3 +4,111 @@
 // License: MIT
 
 #pragma once
+#include <assert.h>
+
+namespace CU {
+    // instructions family
+    // instructions sets
+
+#ifdef CU_ARCH_X86_64
+// see https://www.intel.com/content/www/us/en/docs/intrinsics-guide/index.html
+
+#define INSTRUCTIONS_SETS                               \
+    BEGIN_INSTRUCTIONS_FAMILIES_LIST                    \
+        BEGIN_INSTRUCTIONS_FAMILY(MMX)                  \
+            ADD_INSTRACTIONS_SET(MMX)                   \
+        END_INSTRUCTIONS_FAMILY(MMX)                    \
+        BEGIN_INSTRUCTIONS_FAMILY(SSE)                  \
+            ADD_INSTRACTIONS_SET(SSE)                   \
+            ADD_INSTRACTIONS_SET(SSE2)                  \
+            ADD_INSTRACTIONS_SET(SSE3)                  \
+            ADD_INSTRACTIONS_SET(SSSE3)                 \
+            ADD_INSTRACTIONS_SET(SSE4_1)                \
+            ADD_INSTRACTIONS_SET(SSE4_2)                \
+        END_INSTRUCTIONS_FAMILY(SSE)                    \
+        BEGIN_INSTRUCTIONS_FAMILY(AVX)                  \
+            ADD_INSTRACTIONS_SET(AVX)                   \
+            ADD_INSTRACTIONS_SET(AVX2)                  \
+        END_INSTRUCTIONS_FAMILY(AVX)                    \
+        BEGIN_INSTRUCTIONS_FAMILY(AVX_512)              \
+            ADD_INSTRACTIONS_SET(AVX_512F)              \
+            ADD_INSTRACTIONS_SET(AVX_512PF)             \
+            ADD_INSTRACTIONS_SET(AVX_512ER)             \
+            ADD_INSTRACTIONS_SET(AVX_512CD)             \
+            ADD_INSTRACTIONS_SET(AVX_512BW)             \
+            ADD_INSTRACTIONS_SET(AVX_512DQ)             \
+            ADD_INSTRACTIONS_SET(AVX_512VL)             \
+            ADD_INSTRACTIONS_SET(AVX_512IFMA)           \
+            ADD_INSTRACTIONS_SET(AVX_512VBMI)           \
+            ADD_INSTRACTIONS_SET(AVX_512VBMI2)          \
+            ADD_INSTRACTIONS_SET(AVX_512VNNI)           \
+            ADD_INSTRACTIONS_SET(AVX_512BITALG)         \
+            ADD_INSTRACTIONS_SET(AVX_512BF16)           \
+            ADD_INSTRACTIONS_SET(AVX_512VPOPCNTDQ)      \
+            ADD_INSTRACTIONS_SET(AVX_512VP2INTERSECT)   \
+        END_INSTRUCTIONS_FAMILY(AVX_512)  \
+    END_INSTRUCTIONS_FAMILIES_LIST
+
+#endif // CU_ARCH_X86_64
+
+#ifndef INSTRUCTIONS_SETS
+#error "Instruction sets are not defined for this platform"
+#endif // !INSTRUCTIONS_SETS
+
+// preprocessor magic starts here
+
+// generate instructions family enum
+#define BEGIN_INSTRUCTIONS_FAMILIES_LIST \
+    enum E_INSTRUCTIONS_FAMILY {
+#define     BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
+            E_INFAM_ ##FAM_NAME,
+#define         ADD_INSTRACTIONS_SET(SET_NAME)
+#define     END_INSTRUCTIONS_FAMILY(FAM_NAME)
+#define END_INSTRUCTIONS_FAMILIES_LIST \
+        E_INFAM_COUNT, \
+        E_INFAM_END = E_INFAM_COUNT, \
+        E_INFAM_BEGIN = 0 \
+    };
+    INSTRUCTIONS_SETS
+    static_assert(E_INFAM_COUNT < sizeof(int) * 8, "Too many instructions families defined");
+
+#undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
+#undef   BEGIN_INSTRUCTIONS_FAMILY
+#undef     ADD_INSTRACTIONS_SET
+#undef   END_INSTRUCTIONS_FAMILY
+#undef END_INSTRUCTIONS_FAMILIES_LIST
+
+// generate instructions family support flags
+#define BEGIN_INSTRUCTIONS_FAMILIES_LIST \
+    enum E_INSTRUCTIONS_FAMILY_SUPPORT_FLAG {
+#define     BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
+            E_INFAM_SUPPORT_FLAG_ ##FAM_NAME = 1 << E_INFAM_ ##FAM_NAME,
+#define         ADD_INSTRACTIONS_SET(SET_NAME)
+#define     END_INSTRUCTIONS_FAMILY(FAM_NAME)
+#define END_INSTRUCTIONS_FAMILIES_LIST \
+    };
+    INSTRUCTIONS_SETS
+
+#undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
+#undef   BEGIN_INSTRUCTIONS_FAMILY
+#undef     ADD_INSTRACTIONS_SET
+#undef   END_INSTRUCTIONS_FAMILY
+#undef END_INSTRUCTIONS_FAMILIES_LIST
+
+// generate instructions family string names
+#define BEGIN_INSTRUCTIONS_FAMILIES_LIST \
+    static const char* STR_INSTRUCTIONS_FAMILY[E_INFAM_COUNT] = {
+#define     BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
+            #FAM_NAME,
+#define         ADD_INSTRACTIONS_SET(SET_NAME)
+#define     END_INSTRUCTIONS_FAMILY(FAM_NAME)
+#define END_INSTRUCTIONS_FAMILIES_LIST \
+    };
+    INSTRUCTIONS_SETS
+
+#undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
+#undef   BEGIN_INSTRUCTIONS_FAMILY
+#undef     ADD_INSTRACTIONS_SET
+#undef   END_INSTRUCTIONS_FAMILY
+#undef END_INSTRUCTIONS_FAMILIES_LIST
+}
