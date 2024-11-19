7b6f6fb1bcdc12b70e124196723e41bf6f47e705
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-11-19 22:57:01 +0700

    doc: instructions sets code generation description

--- include/cpu-utils.hpp
+++ include/cpu-utils.hpp
@@ -7,8 +7,27 @@
 #include <assert.h>
 
 namespace CU {
-    // instructions family
-    // instructions sets
+// The supported instruction sets are defined using the INSTRUCTIONS_SETS macro.
+// Custom INSTRUCTIONS_SETS can be used. Due to the limited number of bits in one-hot encoding,
+// the supported instruction sets are divided into families. It is necessary to specify a 
+// list of instruction families and a set of instructions in each family.
+// 
+// Expected structure of the INSTRUCTIONS_SETS macro:
+//  BEGIN_INSTRUCTIONS_FAMILIES_LIST
+//      BEGIN_INSTRUCTIONS_FAMILY(FAMILY_1)
+//          ADD_INSTRACTIONS_SET(SET_1_1, PLATFORM_SPECIFIC_HANDLERS)
+//          ADD_INSTRACTIONS_SET(SET_1_2, PLATFORM_SPECIFIC_HANDLERS)
+//          ...
+//          ADD_INSTRACTIONS_SET(SET_1_A, PLATFORM_SPECIFIC_HANDLERS)
+//      END_INSTRUCTIONS_FAMILY(FAMILY_1)
+//      ...
+//      BEGIN_INSTRUCTIONS_FAMILY(FAMILY_N)
+//          ADD_INSTRACTIONS_SET(SET_N_1, PLATFORM_SPECIFIC_HANDLERS)
+//          ADD_INSTRACTIONS_SET(SET_N_2, PLATFORM_SPECIFIC_HANDLERS)
+//          ...
+//          ADD_INSTRACTIONS_SET(SET_N_C, PLATFORM_SPECIFIC_HANDLERS)
+//      END_INSTRUCTIONS_FAMILY(FAMILY_1)
+//  END_INSTRUCTIONS_FAMILIES_LIST
 
 #ifndef INSTRUCTIONS_SETS
 #ifdef  CU_ARCH_X86_64
@@ -56,12 +75,59 @@ namespace CU {
 // preprocessor magic works here
 #include "code-generators/instructions-sets.h"
 
-// for INSTRUCTIONS_SETS the following objects are generated:
-//  - single enum E_INSTRUCTIONS_FAMILY - list of all instructions families
-//  - single enum E_INSTRUCTIONS_FAMILY_SUPPORT_FLAG - one-hot encoded list of all instructions families
-//  - single static const char* STR_INSTRUCTIONS_FAMILY[] - array of instructions family names
-//  - set of enum E_INSTRUCTIONS_SET_<INSTRUCTIONS_FAMILY_NAME> - list of instructions sets included in the family
-//  - set of enum E_INSTRUCTIONS_SET_<INSTRUCTIONS_FAMILY_NAME>_SUPPORT_FLAG - one-hot encoded list of instructions sets included in the family
-//  - single static const char* STR_INSTRUCTIONS_SETS[][] - array of instructions set names
+// for the given INSTRUCTIONS_SETS the following objects are generated:
+//  1) enum E_INSTRUCTIONS_FAMILY {
+//          E_INFAM_BEGIN,
+//          E_INFAM_<FAMILY_1> = E_INFAM_BEGIN,
+//          E_INFAM_<FAMILY_2>,
+//          ...
+//          E_INFAM_<FAMILY_N>,
+//          E_INFAM_COUNT,
+//          E_INFAM_END = E_INFAM_COUNT
+//     };
+//     Description: List of all instructions families. This object is generated once.
+// 
+//  2) enum E_INSTRUCTIONS_FAMILY_SUPPORT_FLAG {
+//          E_INFAM_SUPPORT_FLAG_<FAMILY_1> = 1 << E_INFAM_<FAMILY_1>,
+//          E_INFAM_SUPPORT_FLAG_<FAMILY_2> = 1 << E_INFAM_<FAMILY_2>,
+//          ...
+//          E_INFAM_SUPPORT_FLAG_<FAMILY_2> = 1 << E_INFAM_<FAMILY_N>,
+//     };
+//     Description: One-hot encoded list of all instructions families. This object is generated once.
+// 
+//  3) static const char* STR_INSTRUCTIONS_FAMILY[] = {
+//          "FAMILY_1", "FAMILY_2", ... "FAMILY_N",
+//     };
+//     Description: Array of instructions family names. This object is generated once.
+// 
+//  4) enum E_INSTRUCTIONS_SET_<FAMILY_K> {
+//          E_INSET_<FAMILY_K>_BEGIN = E_INFAM_<FAMILY_K> << 16,
+//          E_INSET_<SET_K_1> = E_INSET_<FAMILY_K>_BEGIN,
+//          E_INSET_<SET_K_2>,
+//          ...
+//          E_INSET_<SET_K_M>,
+//          E_INSET_<FAMILY_K>_END,
+//          E_INSET_<FAMILY_K>_COUNT = E_INSET_<FAMILY_K>_END - E_INSET_<FAMILY_K>_BEGIN
+//     };
+//     Description: List of instructions sets included in the family.
+//                  A similar object is generated for each instructions family.
+// 
+//  5) enum E_INSTRUCTIONS_SET_<FAMILY_K>_SUPPORT_FLAG {
+//          E_INSET_SUPPORT_FLAG_<SET_K_1> = 1 << (E_INSET_<SET_K_1> & 0xFFFF),
+//          E_INSET_SUPPORT_FLAG_<SET_K_2> = 1 << (E_INSET_<SET_K_2> & 0xFFFF),
+//          ...
+//          E_INSET_SUPPORT_FLAG_<SET_K_M> = 1 << (E_INSET_<SET_K_M> & 0xFFFF),
+//     };
+//     Description: One-hot encoded list of instructions sets included in the family.
+//                  A similar object is generated for each instructions family.
+// 
+//  6) static const char* STR_INSTRUCTIONS_SETS[][] = {
+//          { "SET_1_1", "SET_1_2", ... "SET_1_A" },
+//          { "SET_2_1", "SET_2_2", ... "SET_2_B" },
+//          ...
+//          { "SET_N_1", "SET_N_2", ... "SET_N_C" },
+//     };
+//     Description: Array of instructions set names.
+//                  A similar object is generated for each instructions family.
 
 }
