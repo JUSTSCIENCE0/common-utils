7386452aad526f2e3bee5c446026bea377350075
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-11-19 21:33:36 +0700

    feat: instructions sets string names

--- include/cpu-utils.h
+++ include/cpu-utils.h
@@ -97,7 +97,7 @@ namespace CU {
 #undef   END_INSTRUCTIONS_FAMILY
 #undef END_INSTRUCTIONS_FAMILIES_LIST
 
-// generate instructions family string names
+        // generate instructions family string names
 #define BEGIN_INSTRUCTIONS_FAMILIES_LIST \
     static const char* STR_INSTRUCTIONS_FAMILY[E_INFAM_COUNT] = {
 #define     BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
@@ -106,7 +106,7 @@ namespace CU {
 #define     END_INSTRUCTIONS_FAMILY(FAM_NAME)
 #define END_INSTRUCTIONS_FAMILIES_LIST \
     };
-    INSTRUCTIONS_SETS
+        INSTRUCTIONS_SETS
 
 #undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
 #undef   BEGIN_INSTRUCTIONS_FAMILY
@@ -114,7 +114,7 @@ namespace CU {
 #undef   END_INSTRUCTIONS_FAMILY
 #undef END_INSTRUCTIONS_FAMILIES_LIST
 
-// generate instructions sets enum
+        // generate instructions sets enums
 #define BEGIN_INSTRUCTIONS_FAMILIES_LIST
 #define BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
     enum E_INSTRUCTIONS_SET_ ##FAM_NAME { \
@@ -128,7 +128,26 @@ namespace CU {
     }; \
     static_assert(E_INSET_ ##FAM_NAME ##_COUNT < sizeof(int) * 8, "Too many instructions sets defined for family " #FAM_NAME);
 #define END_INSTRUCTIONS_FAMILIES_LIST
-    INSTRUCTIONS_SETS
+        INSTRUCTIONS_SETS
+
+#undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
+#undef   BEGIN_INSTRUCTIONS_FAMILY
+#undef     ADD_INSTRACTIONS_SET
+#undef   END_INSTRUCTIONS_FAMILY
+#undef END_INSTRUCTIONS_FAMILIES_LIST
+
+// generate instructions sets string names
+#define BEGIN_INSTRUCTIONS_FAMILIES_LIST \
+    static const char* STR_INSTRUCTIONS_SETS[][sizeof(int) * 8 - 1] = {
+#define     BEGIN_INSTRUCTIONS_FAMILY(FAM_NAME) \
+        {
+#define         ADD_INSTRACTIONS_SET(SET_NAME, ...) \
+            #SET_NAME,
+#define     END_INSTRUCTIONS_FAMILY(FAM_NAME) \
+        },
+#define END_INSTRUCTIONS_FAMILIES_LIST \
+    };
+        INSTRUCTIONS_SETS
 
 #undef BEGIN_INSTRUCTIONS_FAMILIES_LIST
 #undef   BEGIN_INSTRUCTIONS_FAMILY
