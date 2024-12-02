44099e8c54a5d457b6ca0a6adb335f003b075746
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-12-02 11:25:27 +0800

    add cli utils framework

---
+++ include/cli-utils.hpp
@@ -0,0 +1,15 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+#include <getopt.h>
+
+namespace CU {
+// TODO: doc - interface
+#ifndef CLI_CONFIGURATION
+
+#endif // !CLI_CONFIGURATION
+}
---
+++ include/code-generators/cli-parsers.h
@@ -0,0 +1,8 @@
+// Copyright (c) 2024, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+// do not use separately from cli-utils.hpp
