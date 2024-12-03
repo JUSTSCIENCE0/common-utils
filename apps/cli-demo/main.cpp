bd224a26ed98a0cb7b5ccc9ad1ef054febe09e2d
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-12-03 23:21:25 +0800

    parser & validators draft

--- apps/cli-demo/main.cpp
+++ apps/cli-demo/main.cpp
@@ -4,6 +4,31 @@
 // License: MIT
 #include "cli-utils.hpp"
 
-int main(/*int argc, char* argv[]*/) {
+#include <vector>
+
+int main(int argc, char* argv[]) {
+    if (argc < 2)
+        return -1;
+
+    using arg_type = long double;
+    std::vector<arg_type> values(argc - 1);
+
+    for (int i = 0; i < argc - 1; i++) {
+        const char* current_arg = argv[i + 1];
+        std::cout << "Argument: \"" << current_arg << "\"" << std::endl;
+
+        if (!CU::parse_option(current_arg, values[i])) {
+            std::cout << "Parse error" << std::endl;
+        }
+
+        std::cout << "Value: " << values[i] << std::endl;
+
+        if (!CU::validate_option(values[i], CU::RangeValidator<arg_type>(0, 50))) {
+            std::cout << "Validation error" << std::endl;
+        }
+
+        std::cout << std::endl;
+    }
+
     return 0;
 }
--- include/cli-utils.hpp
+++ include/cli-utils.hpp
@@ -7,16 +7,70 @@
 
 #include <getopt.h>
 
+#include <cassert>
+#include <sstream>
+#include <iostream>
+#include <concepts>
+
 namespace CU {
-// TODO: doc - interface
+// TODO: doc - CLI_CONFIGURATION interface
 #ifndef CLI_CONFIGURATION
 // TODO: default CLI_CONFIGURATION
 #define CLI_CONFIGURATION
 #endif // !CLI_CONFIGURATION
 
-// TODO: parser template
-//       validator template
-//       template specializations for additional functional
+    // parsing
+    template <typename OptionType>
+    bool parse_option(const char* in_option, OptionType& out_value) {
+        std::stringstream parser(in_option);
+        parser >> out_value;
+
+        if (!parser.eof() || parser.fail())
+            return false;
+        else
+            return true;
+    }
+
+    // validation
+    template <typename OptionType>
+    class BaseValidator {
+    public:
+        virtual bool CheckValue(const OptionType& value) const { return true; }
+    };
+
+    // validator that checks if a value is within the range [min, max]
+    template <std::three_way_comparable OptionType>
+    class RangeValidator final :
+        public BaseValidator<OptionType> {
+    public:
+        RangeValidator(
+            OptionType min,
+            OptionType max) :
+            m_min(min),
+            m_max(max) {
+            assert(min < max);
+        }
+
+        bool CheckValue(const OptionType& value) const override {
+            return (m_min <= value) && (value <= m_max);
+        }
+
+    private:
+        const OptionType m_min;
+        const OptionType m_max;
+    };
+
+#ifdef CUSTOM_VALIDATORS
+    // TODO: doc - CUSTOM_VALIDATORS
+    CUSTOM_VALIDATORS
+#endif CUSTOM_VALIDATORS
+
+    template <typename OptionType>
+    bool validate_option(const OptionType& value, const BaseValidator<OptionType>& validator) {
+        return validator.CheckValue(value);
+    }
+
+// TODO: template specializations for additional functional
 
 // preprocessor magic works here
 #include "code-generators/cli-parsers.h"
