ecd3c3049c3ebb5a49ced4658f2b3a5e0f73d8f8
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-12-12 14:12:06 +0700

    fix linking

--- apps/cli-demo/main.cpp
+++ apps/cli-demo/main.cpp
@@ -17,30 +17,9 @@
 #include <vector>
 
 int main(int argc, char* argv[]) {
-    std::cout << CU::PrivateImplementation::generate_optstring() << std::endl;
-
-    if (argc < 2)
+    CU::CLIConfig cli_config{};
+    if (!CU::parse_cli_args(argc, argv, &cli_config))
         return -1;
-
-    using arg_type = CU::HEX;
-    std::vector<arg_type> values(argc - 1);
-
-    for (int i = 0; i < argc - 1; i++) {
-        const char* current_arg = argv[i + 1];
-        std::cout << "Argument: \"" << current_arg << "\"" << std::endl;
-
-        if (!CU::parse_option(current_arg, &values[i])) {
-            std::cout << "Parse error" << std::endl;
-        }
-
-        std::cout << "Value: " << values[i] << std::endl;
-
-        if (!CU::validate_option(values[i], CU::ListValidator<arg_type>({ 0x123, 0x0, 50 }))) {
-            std::cout << "Validation error" << std::endl;
-        }
-
-        std::cout << std::endl;
-    }
 
     return 0;
 }
--- src/external/getopt_long.c
+++ src/external/getopt_long.c
@@ -56,11 +56,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-int opterr = 1;     /* if error message should be printed */
-int optind = 1;     /* index into parent argv vector */
-int optopt = '?';   /* character checked for validity */
-int optreset;       /* reset getopt */
-char    *optarg;    /* argument associated with option */
+int   opterr = 1;     /* if error message should be printed */
+int   optind = 1;     /* index into parent argv vector */
+int   optopt = '?';   /* character checked for validity */
+int   optreset;       /* reset getopt */
+char* optarg;         /* argument associated with option */
 
 #if 0
 /* DEF_* only work on initialized (non-COMMON) variables */
