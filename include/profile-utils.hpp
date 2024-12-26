6fa34e30037f05d6e073e516bbf20923251bbed8
JUSTSCIENCE0 <yakovmen62@gmail.com>
2024-12-26 23:13:05 +0700

    beautifullization

--- include/profile-utils.hpp
+++ include/profile-utils.hpp
@@ -18,27 +18,29 @@
 // USE_CU_PROFILE
 // CU_PROFILE_CHECKBLOCK(NAME), NAME - optional
 // CU_STOP_CHECKBLOCK(NAME)
+//
 
 #define USE_CU_PROFILE               do { std::cout << "USE_CU_PROFILE" << std::endl; } while(0)
 #define CU_PROFILE_CHECKBLOCK(...)   CU_CHECKBLOCK_MACRO_CHOOSER(__VA_ARGS__)(__VA_ARGS__)
 #define CU_STOP_CHECKBLOCK(NAME)     NAME ##_timer.Stop()
 
-// preproccessor magic
+// preprocessor magic
 #define CU_FUNC_CHOOSER(_f1, _f2, ...) _f2
 #define CU_FUNC_RECOMPOSER(argsWithParentheses) CU_FUNC_CHOOSER argsWithParentheses
 #define CU_CHOOSE_FROM_ARG_COUNT(...) CU_FUNC_RECOMPOSER((__VA_ARGS__, CU_PROFILE_CHECKBLOCK_NAMED, ))
 #define CU_NO_ARG_EXPANDER() ,CU_PROFILE_CHECKBLOCK_ANONYMOUS
 #define CU_CHECKBLOCK_MACRO_CHOOSER(...) CU_CHOOSE_FROM_ARG_COUNT(CU_NO_ARG_EXPANDER __VA_ARGS__ ())
 
-#define CU_PROFILE_CHECKBLOCK_ANONYMOUS()   CU::CheckBlockTimer CU_TIMER_NAME( "", \
-                                            std::string(__FILE__).erase(0, CU_PREFIX_LENGTH + 1), __LINE__, __FUNCTION__);
-#define CU_PROFILE_CHECKBLOCK_NAMED(NAME)  CU::CheckBlockTimer NAME ##_timer( std::string(#NAME) + ": ", \
-                                        std::string(__FILE__).erase(0, CU_PREFIX_LENGTH + 1), __LINE__, __FUNCTION__);
+#define CU_PROFILE_CHECKBLOCK_ANONYMOUS() CU::CheckBlockTimer CU_TIMER_NAME( "", \
+                                              std::string(__FILE__).erase(0, CU_PREFIX_LENGTH + 1), __LINE__, __FUNCTION__);
+#define CU_PROFILE_CHECKBLOCK_NAMED(NAME) CU::CheckBlockTimer NAME ##_timer( std::string(#NAME) + ": ", \
+                                              std::string(__FILE__).erase(0, CU_PREFIX_LENGTH + 1), __LINE__, __FUNCTION__);
 
 #define CU_CONCAT_(lhs, rhs) lhs ## rhs
 #define CU_CONCAT(lhs, rhs) CU_CONCAT_(lhs, rhs)
 #define CU_TIMER_NAME CU_CONCAT(timer_, __LINE__)
 
+// implementation
 namespace CU {
     class CheckBlockTimer {
     public:
