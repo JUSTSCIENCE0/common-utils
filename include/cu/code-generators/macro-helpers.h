aca663e8d0f96c3fee147c828c67cd36022d6c29
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-05-31 22:27:25 +0700

    for_each recursive macro

---
+++ include/cu/code-generators/macro-helpers.h
@@ -0,0 +1,25 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+// do not use separately from macro-utils.hpp
+
+// helpers for recursive CU_FOR_EACH and CU_CONCAT_FOR_EACH
+// see https://www.scs.stanford.edu/~dm/blog/va-opt.html
+#define CU_PARENS ()
+#define CU_EXPAND(...) CU_EXPAND4(CU_EXPAND4(CU_EXPAND4(CU_EXPAND4(__VA_ARGS__))))
+#define CU_EXPAND4(...) CU_EXPAND3(CU_EXPAND3(CU_EXPAND3(CU_EXPAND3(__VA_ARGS__))))
+#define CU_EXPAND3(...) CU_EXPAND2(CU_EXPAND2(CU_EXPAND2(CU_EXPAND2(__VA_ARGS__))))
+#define CU_EXPAND2(...) CU_EXPAND1(CU_EXPAND1(CU_EXPAND1(CU_EXPAND1(__VA_ARGS__))))
+#define CU_EXPAND1(...) __VA_ARGS__
+#define CU_FOR_EACH_HELPER(macro, a1, ...) \
+    macro(a1) \
+    __VA_OPT__(CU_FOR_EACH_AGAIN CU_PARENS (macro, __VA_ARGS__))
+#define CU_FOR_EACH_AGAIN() CU_FOR_EACH_HELPER
+#define CU_CONCAT_FOR_EACH_HELPER(value, a1, ...) \
+    CU_EXPAND_CONCAT(value, a1), \
+    __VA_OPT__(CU_CONCAT_FOR_EACH_AGAIN CU_PARENS (value, __VA_ARGS__))
+#define CU_CONCAT_FOR_EACH_AGAIN() CU_CONCAT_FOR_EACH_HELPER
--- include/cu/macro-utils.hpp
+++ include/cu/macro-utils.hpp
@@ -1,15 +1,26 @@
-// Copyright (c) 2024-2025, Yakov Usoltsev
+// Copyright (c) 2025, Yakov Usoltsev
 // Email: yakovmen62@gmail.com
 //
 // License: MIT
 
 #pragma once
 
-#define CU_CONCAT(lhs, rhs) lhs ##_ ## rhs
+// preprocessor magic works here
+#include <cu/code-generators/macro-helpers.h>
+
+#define CU_CONCAT(lhs, rhs) lhs ##_ ##rhs
 #define CU_EXPAND_CONCAT(lhs, rhs) CU_CONCAT(lhs, rhs)
 
-#define CU_CONT_CONCAT(lhs, rhs) lhs ## rhs
+#define CU_CONT_CONCAT(lhs, rhs) lhs ##rhs
 #define CU_EXPAND_CONT_CONCAT(lhs, rhs) CU_CONT_CONCAT(lhs, rhs)
 
 #define CU_STR(x) #x
 #define CU_EXPAND_STR(x) CU_STR(x)
+
+// thanks to David Mazieres
+// see https://www.scs.stanford.edu/~dm/blog/va-opt.html
+#define CU_FOR_EACH(macro, ...) \
+    __VA_OPT__(CU_EXPAND(CU_FOR_EACH_HELPER(macro, __VA_ARGS__)))
+
+#define CU_CONCAT_FOR_EACH(value, /*postfixes*/...) \
+    __VA_OPT__(CU_EXPAND(CU_CONCAT_FOR_EACH_HELPER(value, __VA_ARGS__)))
