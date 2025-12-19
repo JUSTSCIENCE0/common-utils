3d051e220774ecd41c49c6364b9d392bf57b3fc1
JUSTSCIENCE0 <yakovmen62@gmail.com>
2025-12-19 19:59:52 +0700

    add template utils

---
+++ include/cu/template-utils.hpp
@@ -0,0 +1,32 @@
+// Copyright (c) 2025, Yakov Usoltsev
+// Email: yakovmen62@gmail.com
+//
+// License: MIT
+
+#pragma once
+
+#include <type_traits>
+#include <cstddef>
+
+namespace CU {
+    template<typename T, typename... Ts>
+    constexpr bool contains_v = (std::is_same_v<T, Ts> || ...);
+
+    template<typename T, typename... Ts>
+    struct type_index;
+
+    template<typename T, typename First, typename... Rest>
+    struct type_index<T, First, Rest...> {
+        static constexpr size_t value =
+            std::is_same_v<T, First> ? 0 :
+            1 + type_index<T, Rest...>::value;
+    };
+
+    template<typename T>
+    struct type_index<T> {
+        static_assert(!std::is_same_v<T, T>, "Type not found in type list");
+    };
+
+    template<typename T, typename... Ts>
+    constexpr size_t type_index_v = type_index<T, Ts...>::value;
+}
